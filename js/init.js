b3be25b23bb1da7745bb76a698207c8267ff84f0
XanxusX <tako30215@hotmail.com>
2017-09-18 02:42:34 +0800

    add dropdown menu

--- js/init.js
+++ js/init.js
@@ -6,3 +6,19 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+( function( $ ) {
+  $( document ).ready(function() {
+  $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+    $('#cssmenu #menu-button').on('click', function(){
+      var menu = $(this).next('ul');
+      if (menu.hasClass('open')) {
+        menu.removeClass('open');
+      }
+      else {
+        menu.addClass('open');
+      }
+    });
+  });
+  } )( jQuery );
+  
