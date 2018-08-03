2706ae4273aa42663077d5633ff8e0d76137b500
mark19891107 <mark19891107@gmail.com>
2018-08-03 15:09:18 +0800

    update features

---
+++ _plugins/eval_array.rb
@@ -0,0 +1,10 @@
+module Jekyll
+    module EvalArray
+      def eval_array(text)
+        text2 = text.gsub! '}{', '},{'
+        eval('['+ text2 +']')
+      end
+    end
+  end
+
+  Liquid::Template.register_filter(Jekyll::EvalArray)
