79a12b39364901c7330a59e1ffc53084a14edcb0
billy3321 <billy3321@gmail.com>
2018-09-10 09:31:11 +0800

    fix indent

--- _plugins/eval_array.rb
+++ _plugins/eval_array.rb
@@ -1,10 +1,10 @@
 module Jekyll
-    module EvalArray
-      def eval_array(text)
-        text2 = text.gsub! '}{', '},{'
-        eval('['+ text2 +']')
-      end
+  module EvalArray
+    def eval_array(text)
+      text2 = text.gsub! '}{', '},{'
+      eval('['+ text2 +']')
     end
   end
+end
 
-  Liquid::Template.register_filter(Jekyll::EvalArray)
+Liquid::Template.register_filter(Jekyll::EvalArray)
