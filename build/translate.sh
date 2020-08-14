2f925fd195aa1e0dd4a4030471bf90954206c9a6
bingluen <bingluen0224@gmail.com>
2020-08-14 22:51:03 +0800

    Rewrite translate script

---
+++ build/translate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git remote set-url origin https://$GITHUB_TOKEN@github.com/pdis/web-jekyll
+git checkout master
+npm install
+node build/articles_translate.js
+
+if [ "$(git diff --exit-code)" ]; then
+  git config --global user.email "auto_translate@pdis.tw"
+  git config --global user.name "auto_translate"
+  git add .
+  cat <<EOF |
+Auto translate
+
+Generate follow file:
+$(git diff --staged --name-only)
+EOF
+  git commit -F -
+  git push origin master
+else
+  echo 'there is no changes.'
+fi
